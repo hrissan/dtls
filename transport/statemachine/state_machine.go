cae34f0088adabde1339357245c69a6a31e5777f
Hrissan <hrissan@mail.ru>
2025-08-20 18:12:18 +0300

    comments

--- transport/statemachine/state_machine.go
+++ transport/statemachine/state_machine.go
@@ -24,3 +24,48 @@ func (hctx *handshakeContext) generateFinished(conn *ConnectionImpl) handshake.M
 		Body:    messageBody,
 	}
 }
+
+// If we have no connection (conn == null)
+// client:
+//   * -> ignore, those are either attack or retransmissions from previous connection
+// server:
+//   plaintext ClientHello
+//     w/o cookie or key_share
+//       if params supported
+//         send ServerHelloRetryRequest
+//       if params not supported
+//         send stateless error (correct alert code)
+//     with cookie
+//       old timestamp or invalid cookie
+//         send stateless alert (replay or attack)
+//       correct cookie timestamp
+//         create connection, add to map, create handshake, send ServerHello [statemachine]
+//   plaintext *
+//     send stateless alert (replay or attack)
+//   plaintext ack
+//     ignore
+//   plaintext alert
+//     ignore
+
+// If we have connection, but not yet established (hctx != null)
+// both:
+
+// If we have Connection established (hctx == null), both peers already destroyed their epoch < 3 keys
+// both:
+//   encrypted KeyUpdate -> start key update
+//   encrypted application data -> deliver
+// server: (either attacker, or client who lost/closed association and wants a new connection)
+//   plaintext ClientHello w/o cookie ->
+//     send encrypted empty ack + send ServerHelloRetryRequest, remember cookie timestamp in connection
+//   plaintext ClientHello w old cookie timestamp or invalid cookie ->
+//     replay or attack, do nothing
+//   plaintext ClientHello w correct cookie timestamp ->
+//     destroy connection without sending alert + start handshake
+//     we could have completely separate handshake state machine, so would auth client
+//     before destroying old connection, but that requires 2 complete sets of keys/secrets
+//     and complicated logic, so do it later or never.
+//   plaintext ServerHello -> ignore, send warning
+//   * -> close
+// client:
+//   encrypted NewSessionTicket -> deliver
+//   * -> close

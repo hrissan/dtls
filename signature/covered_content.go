555a3d8fbe77a7809a06a9680c027042f3ef720b
Hrissan <hrissan@mail.ru>
2025-08-30 21:26:31 +0300

    cipersuite refactor

--- signature/covered_content.go
+++ signature/covered_content.go
@@ -5,22 +5,16 @@ package signature
 
 import (
 	"hash"
-
-	"github.com/hrissan/dtls/constants"
 )
 
 // [rfc8446:4.4.3]
-const CoveredContentPrefix = "                                                                " +
-	"TLS 1.3, server CertificateVerify"
+var coveredContentPrefix = []byte("                                                                " +
+	"TLS 1.3, server CertificateVerify\x00")
 
 // like hash.Sum, appends hash to data and returns it
 func CalculateCoveredContentHash(hasher hash.Hash, certVerifyTranscriptHash []byte, data []byte) []byte {
-	var storage [len(CoveredContentPrefix) + 1 + constants.MaxHashLength]byte
+	_, _ = hasher.Write(coveredContentPrefix)
+	_, _ = hasher.Write(certVerifyTranscriptHash)
 
-	sigMessage := append(storage[:0], CoveredContentPrefix...)
-	sigMessage = append(sigMessage, 0)
-	sigMessage = append(sigMessage, certVerifyTranscriptHash...)
-
-	_, _ = hasher.Write(sigMessage)
 	return hasher.Sum(data)
 }
